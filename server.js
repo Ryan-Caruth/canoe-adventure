4de0ddf44fb52284647f45d57d07d4b386696463
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-03-21 22:52:06 -0600

    Added server to new folder

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const res = require("express/lib/response");
+
+const gameRouter = require("./routes/gameRoutes");
+
+const app = express();
+
+const PORT = 5000;
+
+app.listen(PORT, echoPortNumber);
+
+app.use("/api", gameRouter);
+
+function echoPortNumber() {
+  console.log(`Listening on port ${PORT}`);
+}
